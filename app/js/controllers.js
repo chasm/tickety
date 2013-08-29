679151bd68fac4e872a1364657a5b953a13f97e5
chasm <charles.munat@gmail.com>
2013-08-29 14:59:51 -0700

    Messy crap committed for Machiko.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,42 +7,17 @@ angular.
     'fb',
     function($scope, fb) {
     
-      $scope.clearBoard = function() {
+      $scope.startGame = function() {
+        fb.startGame();
         return false;
-      };
-      
-      $scope.setBoard = function() {
-        if ($scope.board) {
-          $scope.cell0 = $scope.board.game.charAt(0);
-          $scope.cell1 = $scope.board.game.charAt(1);
-          $scope.cell2 = $scope.board.game.charAt(2);
-          $scope.cell3 = $scope.board.game.charAt(3);
-          $scope.cell4 = $scope.board.game.charAt(4);
-          $scope.cell5 = $scope.board.game.charAt(5);
-          $scope.cell6 = $scope.board.game.charAt(6);
-          $scope.cell7 = $scope.board.game.charAt(7);
-          $scope.cell8 = $scope.board.game.charAt(8);
-        }
       };
       
     }
   ]).
   controller('BoardCtrl', [
     '$scope',
-    'angularFire',
-    function($scope, angularFire) {
-      var url = 'https://tickety.firebaseio.com/board';
-      var promise = angularFire(url, $scope, 'board', { game: "         " });
-      
-      $scope.makeMove = function(idx) {
-        console.log("making a move in square " + idx);
-        var str = $scope.board.game;
-        $scope.board.game = str.substr(0, idx) + 'X' + str.substr(idx + 1);
-        $scope.setBoard();
-      }
+    'fb',
+    function($scope, fb) {
 
-      promise.then(function() {
-        $scope.$watch('board', $scope.setBoard);
-      });
     }
   ]);
--- app/js/services.js
+++ app/js/services.js
@@ -6,13 +6,17 @@ angular.
     'angularFireCollection',
     '$cookies',
     function(angularFireCollection, $cookies) {
-      this.firebaseRef = new Firebase("https://tickety.firebaseio.com");
+      this.firebaseRef = new Firebase("https://tickety.firebaseio.com/games");
       $cookies.userId = $cookies.userId || Math.uuid(16);
       
-      this.queueRef = this.firebaseRef.child('/queue');
-      this.queue = angularFireCollection(this.queueRef);
-      console.log(this.queue);
-      
+      this.startGame = function() {
+        this.gamesRef = this.firebaseRef.child('/games');
+        this.games = angularFireCollection(this.gamesRef);
+        this.games.push({players: [0]});
+        
+        console.log(this.gamesRef.limit(1).endAt());
+      };
+
       // this.userRef = this.firebaseRef.child('/player/' + $cookies.userId);
       // console.log(this.userRef);
       // this.userInfo = angularFireCollection(this.userRef);
